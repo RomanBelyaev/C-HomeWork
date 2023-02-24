0790ebf91de0f160576bfe2536e74ace7267aefb
Roman Belyaev <Roman-urulga-belyaev@mail.ru>
2023-02-24 18:34:57 +0400

    DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Задача 2: Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+// // a = 5; b = 7 ->  max = 7
+// // a = 2 b = 10 -> max = 10
+// // a = -9 b = -3 -> max = -3
+
+// System.Console.Write( "Введите первое число:");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write( "Введите второе число:");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1 > number2) 
+// {
+// int max = number1;
+// System.Console.WriteLine(max);
+// }
+// else 
+// {
+// int max = number2;   
+// System.Console.WriteLine(max); 
+// }
