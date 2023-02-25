259e8d1f96c9325abf8eb5740dab56b04e7b7042
Roman Belyaev <Roman-urulga-belyaev@mail.ru>
2023-02-25 11:56:55 +0400

    The END

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,8 +1,7 @@
-﻿// // Задача 2: Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+﻿// // Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
 // // a = 5; b = 7 ->  max = 7
 // // a = 2 b = 10 -> max = 10
 // // a = -9 b = -3 -> max = -3
-
 // System.Console.Write( "Введите первое число:");
 // int number1 = Convert.ToInt32(Console.ReadLine());
 // System.Console.Write( "Введите второе число:");
@@ -17,3 +16,57 @@
 // int max = number2;   
 // System.Console.WriteLine(max); 
 // }
+
+// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+// System.Console.Write( "Введите первое число:");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write( "Введите второе число:");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write( "Введите третье число:");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// if (number1 > number2 && number1 > number3 ) 
+// {
+// int max = number1;
+// System.Console.WriteLine(max);
+// }
+// else 
+// if (number2 > number1 && number2 > number3 ) 
+// {
+// int max = number2;   
+// System.Console.WriteLine(max); 
+// }
+// else 
+// if (number3 > number1 && number3 > number2 ) 
+// {
+// int max = number3;   
+// System.Console.WriteLine(max); 
+// }
+
+// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка)
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+// System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if ( number % 2 == 0)
+// {
+// System.Console.WriteLine("Yes");  
+// }
+// else
+// {
+// System.Console.WriteLine("No");
+// }
+
+
+// // Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// // 5 -> 2, 4
+// // 8 -> 2, 4, 6, 8
+// System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if ( number > 1; number = number + 2 )
+// int x = number;
+// System.Console.WriteLine( "x" );
+
